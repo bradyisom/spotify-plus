e61044110074421a86117690359b393b3fad455e
Brady Isom <bradyisom@gmail.com>
2017-08-08 22:53:47 -0600

    feat: initial mix playlist creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MdButtonModule, MdInputModule, MdCheckboxModule, MdIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MdButtonModule,
+    MdInputModule,
+    MdCheckboxModule,
+    MdIconModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+
+import * as SpotifyWebApi from 'spotify-web-api-js';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  public form: FormGroup;
+  public newPlaylist: any;
+
+  private spotify = new SpotifyWebApi();
+  private userId: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      token: '',
+      mixName: `My mix playlist`,
+    });
+  }
+
+  public loadPlaylists() {
+    this.spotify.setAccessToken(this.form.value.token);
+
+    this.spotify.getMe().then((user) => {
+      this.userId = user.id;
+    });
+
+    this.spotify.getUserPlaylists().then((playlists) => {
+      this.form.addControl('playlists', this.formBuilder.array(_.map(playlists.items, (playlist) => {
+        return this.formBuilder.group({
+          id: playlist.id,
+          name: playlist.name,
+          include: false
+        });
+      })));
+      // this.playlists = playlists.items;
+    });
+  }
+
+  get playlists(): FormArray {
+    return this.form.get('playlists') as FormArray;
+  }
+
+  public createMix() {
+    let tracks: any[] = [];
+
+    const trackPromises = _.map(_.filter(this.playlists.controls, (playlist) => {
+      return playlist.value.include;
+    }), (playlist) => {
+      return this.spotify.getPlaylistTracks(this.userId, playlist.value.id).then((playlistTracks) => {
+        tracks = tracks.concat(_.map(playlistTracks.items, track => track.track.id));
+      });
+    });
+
+    let newId: string;
+    Promise.all(trackPromises).then(() => {
+      tracks = _.uniq(_.shuffle(tracks));
+      return this.spotify.createPlaylist(this.userId, {
+        name: this.form.get('mixName').value
+      });
+    }).then((newPlaylist) => {
+      newId = newPlaylist.id;
+      const trackGroups = _.chunk(tracks, 100);
+      let promise = this.addTrackGroup(newId, trackGroups[0]);
+      _.slice(trackGroups, 1).forEach((group) => {
+        promise = this.addTrackGroup(newId, group);
+      });
+      return promise;
+    }).then(() => {
+      return this.spotify.getPlaylist(this.userId, newId);
+    }).then((newPlaylist) => {
+      this.newPlaylist = newPlaylist;
+    });
+  }
+
+  private addTrackGroup(playlistId, group): Promise<any> {
+    return this.spotify.addTracksToPlaylist(this.userId, playlistId, _.map(group, (track) => {
+      return `spotify:track:${track}`;
+    }));
+  }
+}
